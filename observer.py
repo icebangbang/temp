1ccae31c5b443cd327e581be570f31fecbd93d77
icebangbang <lifeng.ye@ecreditpal.com>
2017-08-31 16:13:00 +0800

    init endpoint: search_by_mobile

--- observer.py
+++ observer.py
@@ -2,16 +2,16 @@ from flask import Flask
 from util import rest
 from flask import send_file
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder="static")
 
 
-@app.route('/')
-def hello_world():
+@app.route('/search/<string:mobile>')
+def search_by_mobile(mobile):
     return rest.response_to("hello")
 
-@app.route('/<string:name>')
-def watch(name):
-    return send_file('static/'+name)
+# @app.route('/<string:name>')
+# def watch(name):
+#     return send_file('static/'+name)
 
 if __name__ == '__main__':
     app.run(threaded=True)
